90687ecad16e0f5474bbfd5a3418bf7ec56a1540
khilendra2003-dewangan <khilendra24dewangan@gmail.com>
2026-01-06 16:11:52 +0530

    Fix syntax in sendmail.js

--- backend/src/config/sendmail.js
+++ backend/src/config/sendmail.js
@@ -2,7 +2,9 @@ import { createTransport } from "nodemailer"
 
 const sendMail = async ({ email, subject, html }) => {
     const transporter = createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.USER,
             pass: process.env.PASSWORD
