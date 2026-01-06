f686935d6370c06c67e768cc5a5d2b8ab894b336
khilendra2003-dewangan <khilendra24dewangan@gmail.com>
2026-01-06 18:17:15 +0530

    Revert to Nodemailer with Gmail SMTP

--- backend/src/config/sendmail.js
+++ backend/src/config/sendmail.js
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer';
 
 const sendMail = async ({ email, subject, html }) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.USER,
             pass: process.env.PASSWORD
