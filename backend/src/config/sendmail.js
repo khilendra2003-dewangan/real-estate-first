7f25782cc2761f24b5c3ac55467e6cb7dd887901
khilendra2003-dewangan <khilendra24dewangan@gmail.com>
2026-01-06 16:03:20 +0530

    Fix SMTP timeout by using service: gmail

--- backend/src/config/sendmail.js
+++ backend/src/config/sendmail.js
@@ -1,17 +1,16 @@
 import { createTransport } from "nodemailer"
 
-const sendMail=async({email,subject,html})=>{
-    const transporter=createTransport({
-        host:"smtp.gmail.com",
-        port:465,
-        auth:{
-            user:process.env.USER,
-            pass:process.env.PASSWORD
+const sendMail = async ({ email, subject, html }) => {
+    const transporter = createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD
         }
     })
     await transporter.sendMail({
-        from:process.env.USER,
-        to:email,
+        from: process.env.USER,
+        to: email,
         subject,
         html,
     })
